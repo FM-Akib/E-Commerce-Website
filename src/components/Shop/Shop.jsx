6ab767b33914aee83a78e44aa5958ae92a3dff5c
FM-Akib <muntasirakib08@gmail.com>
2024-01-17 11:21:33 +0600

    updated

---
+++ src/components/Shop/Shop.jsx
@@ -0,0 +1,17 @@
+// import React from 'react';
+import './Shop.css';
+const Shop = () => {
+    return (
+        <div className="shop">
+            <div className="shop-items">
+              left
+
+            </div>
+            <div className="orders">
+right
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
